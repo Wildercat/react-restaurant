3cdc1c95db431df8ad751be74a249e7e181816b7
Wildercat <abramgornik@gmail.com>
2019-10-23 08:14:49 -0400

    Updates

---
+++ precache-manifest.be6be966031861db42dc756cecff0066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793aea8badc2a169fb4baa83a5fcb59d",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "85f43d2bc5be2e42b7fd",
+    "url": "/react-restaurant/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2927ff76b444b2ddb2f8",
+    "url": "/react-restaurant/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "85f43d2bc5be2e42b7fd",
+    "url": "/react-restaurant/static/js/2.f6284b5b.chunk.js"
+  },
+  {
+    "revision": "2927ff76b444b2ddb2f8",
+    "url": "/react-restaurant/static/js/main.1e133979.chunk.js"
+  },
+  {
+    "revision": "2650c159c409280e9da5",
+    "url": "/react-restaurant/static/js/runtime-main.4681381b.js"
+  }
+]);
